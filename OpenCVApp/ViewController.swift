ad0748c9cf5cb4e84fc732dcae73205daa1067d3
di-saldana <dsl42@alu.ua.es>
2024-05-26 13:25:06 +0200

    Added: UI Elements

--- OpenCVApp/ViewController.swift
+++ OpenCVApp/ViewController.swift
@@ -7,8 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController {    
+    @IBOutlet weak var cameraView: UIImageView!
+    @IBOutlet weak var blurSlider: UISlider!
+    @IBOutlet weak var edgeSlider: UISlider!
+    @IBOutlet weak var angleSlider: UISlider!
+    @IBOutlet weak var applyFilterBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
